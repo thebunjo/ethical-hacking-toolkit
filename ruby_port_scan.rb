d6a20d1ca4ffa7ed7c82aeed27b70188884eaf92
thebunjo <138582603+thebunjo@users.noreply.github.com>
2023-11-15 14:59:28 +0300

    Update ruby_port_scan.rb

--- ruby_port_scan.rb
+++ ruby_port_scan.rb
@@ -1,27 +1,45 @@
 require 'socket'
 
-def scan(host, port)
+def port_scan(host, port, timeout_seconds = 1)
+  socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+  sockaddr = Socket.sockaddr_in(port, host)
+
   begin
-    sock = TCPSocket.new(host, port)
-    sock.close
-    puts "[+] Port #{port} is open on #{host}."
+    socket.connect_nonblock(sockaddr)
+    puts "#{port} Open."
+  rescue IO::WaitWritable
+    IO.select(nil, [socket], nil, timeout_seconds)
+    retry
+  rescue Errno::EISCONN
+    puts "#{port} Open."
   rescue Errno::ECONNREFUSED
-    puts "[-] Port #{port} is closed on #{host}."
+    puts "#{port} Closed (Error: Connection refused)."
+  rescue Errno::ETIMEDOUT
+    puts "#{port} Closed (Error: Connection timed out)."
   rescue Errno::EHOSTUNREACH
-    puts "[-] #{port} is unreachable on #{host}."
-  rescue SocketError => e
-    puts "[-] Error occurred while connecting to #{host}: #{e.message}"
+    puts "#{port} Closed (Error: Host unreachable)."
+  rescue Errno::ENETUNREACH
+    puts "#{port} Closed (Error: Network unreachable)."
+  rescue Errno::EINVAL
+    puts "#{port} Closed (Error: Invalid argument)."
+  rescue Exception => e
+    puts "#{port} closed: #{e.message}"
+  ensure
+    socket&.close
   end
 end
 
-host = ARGV[0]
-ports = ARGV[1].split(",")
-threads = []
+def port_scan_with_threads(host, port_list, timeout_seconds = 1)
+  threads = []
 
-ports.each do |port|
-  threads << Thread.new do
-    result = scan(host, port)
+  port_list.each do |port|
+    threads << Thread.new { port_scan(host, port, timeout_seconds) }
   end
+
+  threads.each(&:join)
 end
 
-threads.each(&:join)
+host = 'target.com'
+port_list = [21, 22, 23, 25, 53, 80, 139, 443, 445, 3306, 8080, 3389, 5900, 8081]
+
+port_scan_with_threads(host, port_list, 1)
